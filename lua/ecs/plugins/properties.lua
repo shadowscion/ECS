50cbe90b49df74bbe1cabd7c1f4b55972a82db7c
shadowscion <shadowscion@users.noreply.github.com>
2022-07-20 20:34:12 -0500

    Update properties.lua

--- lua/ecs/plugins/properties.lua
+++ lua/ecs/plugins/properties.lua
@@ -1,6 +1,139 @@
 
 ECS.Plugin = "properties"
 
+--[[
+    model
+    drag
+    shadow
+]]
+
+--
+ECS:AddCommand( "setphysprop", "material", function( ply, trace, params )
+    local mat = params[1]
+
+    local ents = ECS.GetSelection( ply )
+
+    if next( ents ) == nil then
+        local ent = trace.Entity
+
+        if ECS.CanTool( ply, ent, "physprop" ) then
+            local phys = ent:GetPhysicsObject()
+
+            if IsValid( phys ) then
+                construct.SetPhysProp( ply, ent, 0, nil, { GravityToggle = phys:IsGravityEnabled(), Material = mat } )
+            end
+        end
+
+        return
+    end
+
+    for sel, data in pairs( ents ) do
+        if ECS.CanTool( ply, sel, "physprop" ) then
+            local phys = sel:GetPhysicsObject()
+
+            if IsValid( phys ) then
+                construct.SetPhysProp( ply, sel, 0, nil, { GravityToggle = phys:IsGravityEnabled(), Material = mat } )
+            end
+        end
+    end
+end )
+
+
+--
+ECS:AddCommand( "setgravity", "enabled", function( ply, trace, params )
+    local grav = tobool( params[1] )
+
+    local ents = ECS.GetSelection( ply )
+
+    if next( ents ) == nil then
+        local ent = trace.Entity
+
+        if ECS.CanTool( ply, ent, "physprop" ) then
+            local phys = ent:GetPhysicsObject()
+
+            if IsValid( phys ) then
+                construct.SetPhysProp( ply, ent, 0, nil, { GravityToggle = grav, Material = phys:GetMaterial() } )
+            end
+        end
+
+        return
+    end
+
+    for sel, data in pairs( ents ) do
+        if ECS.CanTool( ply, sel, "physprop" ) then
+            local phys = sel:GetPhysicsObject()
+
+            if IsValid( phys ) then
+                construct.SetPhysProp( ply, sel, 0, nil, { GravityToggle = grav, Material = phys:GetMaterial() } )
+            end
+        end
+    end
+end )
+
+
+--
+ECS:AddCommand( "setmass", "mass", function( ply, trace, params )
+    local mass = math.Clamp( tonumber( params[1] ) or 0.1, 0.1, 50000 )
+
+    local ents = ECS.GetSelection( ply )
+
+    if next( ents ) == nil then
+        local ent = trace.Entity
+
+        if ECS.CanTool( ply, ent, "weight" ) then
+            local phys = ent:GetPhysicsObject()
+
+            if IsValid( phys ) then
+                phys:SetMass( mass )
+                duplicator.StoreEntityModifier( ent, "mass", { Mass = mass } )
+            end
+        end
+
+        return
+    end
+
+    for sel, data in pairs( ents ) do
+        if ECS.CanTool( ply, sel, "weight" ) then
+            local phys = sel:GetPhysicsObject()
+
+            if IsValid( phys ) then
+                phys:SetMass( mass )
+                duplicator.StoreEntityModifier( sel, "mass", { Mass = mass } )
+            end
+        end
+    end
+end )
+
+
+--
+ECS:AddCommand( "setmaterial", "material", function( ply, trace, params )
+    local mat = params[1]
+
+    if not game.SinglePlayer() and not list.Contains( "OverrideMaterials", mat ) and mat ~= "" then
+        return
+    end
+
+    local ents = ECS.GetSelection( ply )
+
+    if next( ents ) == nil then
+        local ent = trace.Entity
+
+        if ECS.CanTool( ply, ent, "material" ) then
+            ent:SetMaterial( mat )
+            duplicator.StoreEntityModifier( ent, "material", { MaterialOverride = mat } )
+        end
+
+        return
+    end
+
+    for sel, data in pairs( ents ) do
+        if ECS.CanTool( ply, sel, "material" ) then
+            data.Material = mat
+            duplicator.StoreEntityModifier( sel, "material", { MaterialOverride = mat } )
+        end
+    end
+end )
+
 
 --
 ECS:AddCommand( "setcolor", "r g b a", function( ply, trace, params )
@@ -32,6 +165,39 @@ ECS:AddCommand( "setcolor", "r g b a", function( ply, trace, params )
             data.Color = color
             data.Mode = mode
             duplicator.StoreEntityModifier( sel, "colour", { Color = color, RenderMode = mode } )
+        end
+    end
+end )
+
+
+--
+ECS:AddCommand( "setalpha", "a", function( ply, trace, params )
+    local alpha = tonumber( params[1] ) or 255
+
+    local mode = alpha < 255 and RENDERMODE_TRANSCOLOR or RENDERMODE_NORMAL
+
+    local ents = ECS.GetSelection( ply )
+
+    if next( ents ) == nil then
+        local ent = trace.Entity
+
+        if ECS.CanTool( ply, ent, "colour" ) then
+            local color = ent:GetColor()
+            color.a = alpha
+
+            ent:SetColor( color )
+            ent:SetRenderMode( mode )
+            duplicator.StoreEntityModifier( ent, "colour", { Color = color, RenderMode = mode } )
+        end
+
+        return
+    end
+
+    for sel, data in pairs( ents ) do
+        if ECS.CanTool( ply, sel, "colour" ) then
+            data.Color.a = alpha
+            data.Mode = mode
+            duplicator.StoreEntityModifier( sel, "colour", { Color = data.Color, RenderMode = mode } )
         end
     end
 end )
